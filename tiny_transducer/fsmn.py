34a4adc3925cd5878696d74790a99f14aadce206
yinruiqing <yinruiqing110@gmail.com>
2022-08-04 23:55:32 +0800

    fix: dfsm skip connection.

--- tiny_transducer/fsmn.py
+++ tiny_transducer/fsmn.py
@@ -22,14 +22,17 @@ class CVFSMNv2(nn.Module):
         nn.init.ones_(self._bias2)
         nn.init.xavier_uniform_(self.memory_weights)
 
-    def forward(self, input_data, memory_weight):
-        if memory_weight is not None:
-            self.memory_weights.data = self.memory_weights + memory_weight
+    def forward(self, input_data, skip_conn):
+
         p = torch.matmul(input_data, self._W1) + self._bias1
         p_T = F.pad(p.transpose(1, 2), (self.memory_size - 1, 0))
-        p = F.conv1d(p_T, self.memory_weights, groups=self.projection_size).transpose(1, 2)
-        p = torch.matmul(p, self._W2) + self._bias2
-        return p, self.memory_weights
+        h_hat = F.conv1d(p_T, self.memory_weights, groups=self.projection_size).transpose(1, 2)
+        if skip_conn is not None:
+            p_hat = h_hat + skip_conn
+        else:
+            p_hat = h_hat
+        h = torch.matmul(p, self._W2) + self._bias2
+        return h, p_hat
 
 
 class FSMNBlock(nn.Module):
@@ -38,8 +41,8 @@ class FSMNBlock(nn.Module):
         self.fsmn = CVFSMNv2(memory_size, input_size, output_size, projection_size)
         self.norm = nn.LayerNorm(output_size)
 
-    def forward(self, x, memory_weights=None):
-        out, memory_weights = self.fsmn(x, memory_weights)
+    def forward(self, x, skip_conn=None):
+        out, memory_weights = self.fsmn(x, skip_conn)
         out = self.norm(out)
         return out, memory_weights
 
@@ -52,10 +55,10 @@ class DFSMN(nn.Module):
         self.fsmn_layers = self._make_layers(n_layers - 1, memory_size, hidden_size, output_size, projection_size)
 
     def forward(self, x):
-        memory_weights = None
-        x, memory_weights = self.layer1(x, memory_weights)
+        skip_conn = None
+        x, skip_conn = self.layer1(x, skip_conn)
         for layer in self.fsmn_layers:
-            x, memory_weights = layer(x, memory_weights)
+            x, memory_weights = layer(x, skip_conn)
         return x
 
     def _make_layers(self, num_block, memory_size, hidden_size, output_size, projection_size):
